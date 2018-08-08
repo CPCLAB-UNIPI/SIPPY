b3032ce4e6f000cd7240c1c2757f422f4066ef7a
Cameron Devine <camerondtrain@gmail.com>
2018-08-08 13:47:19 -0700

    added the readme as the long description

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
 
+with open('README.md', 'r') as f:
+	readme = f.read()
+
 setuptools.setup(
 	name="SIPPY",
 	version="0.1.0",
 	author="Giuseppe Armenise",
 	description="Systems Identification Package for Python",
+	long_description=readme
+	long_description_content_type='text/markdown'
 	url="https://github.com/CPCLAB-UNIPI/SIPPY",
 	packages=setuptools.find_packages(),
-	python_requires=">=2.7",
+	python_requires="==2.7,>=3.5,<=3.6",
 	install_requires=(
 		"numpy",
 		"scipy",
@@ -20,6 +25,7 @@ setuptools.setup(
 		"Intended Audience :: Science/Research",
 		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
 		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
 		"Operating System :: OS Independent",
 	),
