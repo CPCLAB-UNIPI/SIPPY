55d4194e821c2a016f73d7235e2f0198d91257b9
jamestjsp <53532429+jamestjsp@users.noreply.github.com>
2021-04-24 10:14:24 +0530

    Update setup.py
    Change to for python version support.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url="https://github.com/CPCLAB-UNIPI/sippy",
 	packages=setuptools.find_packages(),
-	python_requires="==2.7,>=3.5,<=3.7",
+	python_requires="==2.7,<=3.5,>=3.7",
 	install_requires=(
 		"numpy",
 		"scipy",
