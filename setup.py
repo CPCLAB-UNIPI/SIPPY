b699ab99d4f5a0c74941e1752f460241ce4744ab
Marco Vaccari <>
2021-07-09 12:30:31 +0200

    Update setup.py
    Modified versions in python_requires

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url="https://github.com/CPCLAB-UNIPI/sippy",
 	packages=setuptools.find_packages(),
-	python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<=3.7.*", 
+	python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<=3.8",
 	install_requires=(
 		"numpy",
 		"scipy",
@@ -28,7 +28,7 @@ setuptools.setup(
 		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
-        	"Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.7",
 		"Operating System :: OS Independent",
 	),
 )
